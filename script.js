dedcbe06120e1f5cbec521eb12bc102d26919ecb
Roman Bednarz <roman.bednarz@gmail.com>
2019-06-21 01:23:47 +0200

    more counters

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var Counter = React.createClass({
             counter: 0
         };
     },
-
     increment: function() {
         this.setState({
             counter: this.state.counter + 1
@@ -17,13 +16,26 @@ var Counter = React.createClass({
     },
     render: function() {
         return React.createElement('div', {},
-            React.createElement('span', {}, 'Licznik ' + this.state.counter), 
-            React.createElement('button', {onClick: this.increment}, 'zwiększ o 1'),
-            React.createElement('button', {onClick: this.decrement}, 'zmniejsz o 1')
+                React.createElement('span', {}, 'Licznik ' + this.state.counter), 
+                React.createElement('br', {}),
+                React.createElement('button', {onClick: this.increment}, 'zwiększ o 1'),
+                React.createElement('br', {}),
+                React.createElement('button', {onClick: this.decrement}, 'zmniejsz o 1')
         );
     }
 });
+var Counter2 = React.createClass({
+    render: function() {
+        return React.createElement('div', {},
+                    React.createElement(Counter, {}),
+                    React.createElement(Counter, {}),
+                    React.createElement(Counter, {}),
+                    React.createElement(Counter, {}),
+                    React.createElement(Counter, {}),
 
+            )
+    }        
+});
 
-var element = React.createElement(Counter);
+var element = React.createElement(Counter2);
 ReactDOM.render(element, document.getElementById('app'));
